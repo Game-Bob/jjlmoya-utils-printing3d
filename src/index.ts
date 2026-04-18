7eb1380d3ad3484d4fe878918666fe5dd9236006
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:55 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,10 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export { PrintingCostCalculatorComponent, PrintingCostCalculatorSEO, PrintingCostCalculatorBibliography, PrintingCostCalculatorFAQ } from './tool/printingCostCalculator';
 export { PRINTING_COST_CALCULATOR_TOOL } from './tool/printingCostCalculator/index';
 
-export { ShrinkageCalculatorComponent, ShrinkageCalculatorSEO, ShrinkageCalculatorBibliography, ShrinkageCalculatorFAQ } from './tool/shrinkageCalculator';
 export { SHRINKAGE_CALCULATOR_TOOL } from './tool/shrinkageCalculator/index';
 
-export { FlowCalculatorComponent, FlowCalculatorSEO, FlowCalculatorBibliography, FlowCalculatorFAQ } from './tool/flowCalculator';
 export { FLOW_CALCULATOR_TOOL } from './tool/flowCalculator/index';
 
-export { ResinCuringCalculatorComponent, ResinCuringCalculatorSEO, ResinCuringCalculatorBibliography, ResinCuringCalculatorFAQ } from './tool/resinCuringCalculator';
 export { RESIN_CURING_CALCULATOR_TOOL } from './tool/resinCuringCalculator/index';
